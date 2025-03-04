39a570b3455fca80cd52c8866d8f6519514a6de6
Park Sungsoo <150274144+wonderfulawsome@users.noreply.github.com>
2025-03-04 18:23:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GeminiPage from './GeminiPage';
 import KRWordRankPage from './KRWordRankPage';
+import YakePage from './YakePage';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<GeminiPage />} />
         <Route path="/kr-wordrank" element={<KRWordRankPage />} />
+        <Route path="/yake" element={<YakePage />} />
       </Routes>
     </Router>
   );
